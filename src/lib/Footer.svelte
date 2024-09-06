ed4eae9863f12da97c310bb8a08cd0056613d925
Lukas-i-bastun <lukash@live.se>
2024-09-06 12:20:26 +0200

    update

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { authStore, authHandlers } from '../stores/authStore';
+	import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
 
 	let currentUser;
 	authStore.subscribe((curr) => {
@@ -13,12 +14,24 @@
 
 </script>
 
-<section class="bg-slate-300 py-16 link">
-	{#if currentUser}
-	<a on:click={logout}>Logga ut</a>
-	{:else}
-	<a href="/login">Logga in</a>
-	{/if}
+<section class="bg-slate-300 py-16 flex justify-evenly items-center">
+
+	<div class="text-start	">
+		<p>Email: <a href="mailto:swag.goteborg@gmail.com">swag.goteborg@gmail.com</a></p>
+		<p class="">Telefon: <a href="tel: 0709301799">0709301799</a></p>
+		<p>
+			{#if currentUser}
+			<a on:click={logout}>Logga ut</a>
+			{:else}
+			admin: <a href="/login">Logga in</a>
+			{/if}
+		</p>
+	</div>
+	<div class="w-9">
+		<a href="https://www.instagram.com/swag_streetworkout/" target="_blank"><FaInstagram /></a>
+		
+	</div>
+
 </section>
 
 <style>
@@ -31,5 +44,9 @@
 		background-color: #303030;
 		text-align: center;
 		margin-top: 100px;
+	}
+
+	a {
+		text-decoration: underline;
 	}
 </style>
