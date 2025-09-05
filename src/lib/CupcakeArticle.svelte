30ba5856a6597e77aee660afb46941406f343104
Lukas-i-bastun <lukash@live.se>
2025-09-05 14:22:29 +0200

    Table uppdatering

--- src/lib/CupcakeArticle.svelte
+++ src/lib/CupcakeArticle.svelte
@@ -1,7 +1,22 @@
 
 <article
 		data-theme="cupcake"
-		class="container container-bg flex flex-col items-center text-center m-auto my-6 p-6 rounded w-screen lg:w-10/12 xl:w-3/3"
+		id="cupcake-id"
+		class="container container-bg flex flex-col items-center text-center m-auto my-6 rounded w-screen"
 	>
 <slot></slot>
 </article>
+
+<style>
+/* På telefon (små skärmar) */
+  #cupcake-id {
+    width: 100vw;
+  }
+
+/* På stora skärmar */
+@media (min-width: 1200px) {
+  #cupcake-id {
+    width: 70vw;
+  }
+}
+</style>
--- src/lib/tables/FirebaseTable.svelte
+++ src/lib/tables/FirebaseTable.svelte
@@ -1,106 +1,123 @@
 <script lang="ts">
-	import CmsAddTableRow from "../CMS/CmsAddTableRow.svelte";
-	import CmsRemoveTableRow from "../CMS/CmsRemoveTableRow.svelte";
-	import CmsUpdateTableData from "../CMS/CmsUpdateTableData.svelte";
+	import CmsAddTableRow from '../CMS/CmsAddTableRow.svelte';
+	import CmsRemoveTableRow from '../CMS/CmsRemoveTableRow.svelte';
+	import CmsUpdateTableData from '../CMS/CmsUpdateTableData.svelte';
 	import { authStore } from '../../stores/authStore';
 
 	export let tableContent: any;
-	export let tableIndexToUpdate: number
-	export let FBData: any
-	export let getData: (() => void);
+	export let tableIndexToUpdate: number;
+	export let FBData: any;
+	export let getData: () => void;
 
 	let currentUser: object | null = null;
-    authStore.subscribe((curr) => {
-      currentUser = curr?.currentUser;
-    });
+	authStore.subscribe((curr) => {
+		currentUser = curr?.currentUser;
+	});
 
-	const headers = tableContent.headers	
-	const rows = tableContent.rows
+	const headers = tableContent.headers;
+	const rows = tableContent.rows;
 
-	
+	// Byter rad efter X antal ord
+
+	function insertLineBreaks(text, wordsPerLine = 3) {
+		return text
+			.split(' ')
+			.map((word, i) => (i > 0 && i % wordsPerLine === 0 ? '<br>' + word : word))
+			.join(' ');
+	}
+
 </script>
 
-<div data-theme="retro" class="overflow-x-auto my-6 z-0">
-	<table class="table w-full">
-		<thead>
-			<tr>
-				{#if (headers !== undefined)}
-					{#each headers as header}
-						<th class="p-rel text-sm md:text-base">{header}</th>
-					{/each}
-					{#if (currentUser)}
-						<th class="p-rel bg-red text-sm md:text-base">Ta bort rad</th>
-					{/if}
+<div data-theme="retro" class="overflow-x-auto my-2 z-0">
+	<table class="table w-full sm:w-[95vw] table-fixed">
+	<thead>
+		<tr>
+			{#if headers !== undefined}
+				{#each headers as header, headerIndex}
+					<th
+						class={`p-rel text-sm md:text-base ${headerIndex === 0 ? 'w-[37%]' : headerIndex === 1 ? 'w-[73%]' : ''}`}
+					>
+						{header}
+					</th>
+				{/each}
+				{#if currentUser}
+					<th class="p-rel bg-red text-sm md:text-base">Ta bort rad</th>
 				{/if}
-			</tr>
-		</thead>
-		<tbody>
-			{#if (rows !== undefined)}
+			{/if}
+		</tr>
+	</thead>
+	<tbody>
+		{#if rows !== undefined}
 			{#each rows as row, rowIndex}
 				<tr>
 					{#each row.columns as column, columnIndex}
 						{#if column.type === 'text'}
-							<td class="bg-zinc-50 text-sm md:text-base">
-									{column.text}
-									<CmsUpdateTableData 
-										FBData={FBData}
-										FBDocument={'kurser'}
-										tableIndexToUpdate={tableIndexToUpdate}
-										columnIndexToUpdate={columnIndex}
-										rowIndexToUpdate={rowIndex}
-										initialValue={column.text}
-										initialHrefValue={column.href}
-										getData={getData}
-										type={'text'}
-									/>
+							<td
+								class={`bg-zinc-50 text-sm md:text-base ${columnIndex === 0 ? 'w-[37%]' : columnIndex === 1 ? 'w-[73%]' : ''}`}
+							>
+								{@html insertLineBreaks(column.text, columnIndex == 0 ? 2 : 3)}
+								<CmsUpdateTableData
+									{FBData}
+									FBDocument={'kurser'}
+									{tableIndexToUpdate}
+									columnIndexToUpdate={columnIndex}
+									rowIndexToUpdate={rowIndex}
+									initialValue={column.text}
+									initialHrefValue={column.href}
+									{getData}
+									type={'text'}
+								/>
 							</td>
 						{:else if column.type === 'link'}
-							<td class="bg-zinc-50 link text-sm md:text-base">
+							<td
+								class={`bg-zinc-50 link text-sm md:text-base ${columnIndex === 0 ? 'w-[37%]' : columnIndex === 1 ? 'w-[73%]' : ''}`}
+							>
 								<a target="_blank" rel="noreferrer" href={column.href}>
-									{column.text}
+									{@html insertLineBreaks(column.text, columnIndex == 0 ? 2 : 3)}
 								</a>
-								<CmsUpdateTableData 
-										FBData={FBData}
-										FBDocument={'kurser'}
-										tableIndexToUpdate={tableIndexToUpdate}
-										columnIndexToUpdate={columnIndex}
-										rowIndexToUpdate={rowIndex}
-										initialValue={column.text}
-										initialHrefValue={column.href}
-										getData={getData}
-										type={'link'}
-									/>
-							</td>				
+								<CmsUpdateTableData
+									{FBData}
+									FBDocument={'kurser'}
+									{tableIndexToUpdate}
+									columnIndexToUpdate={columnIndex}
+									rowIndexToUpdate={rowIndex}
+									initialValue={column.text}
+									initialHrefValue={column.href}
+									{getData}
+									type={'link'}
+								/>
+							</td>
 						{/if}
 					{/each}
-					{#if (currentUser)}
+					{#if currentUser}
 						<td class="bg-red-50 text-xs md:text-base">
-							<CmsRemoveTableRow 
-							FBData={FBData}
-							FBDocument={'kurser'}
-							tableIndexToUpdate={tableIndexToUpdate}
-							rowIndexToRemove={rowIndex}
-							getData={getData}
+							<CmsRemoveTableRow
+								{FBData}
+								FBDocument={'kurser'}
+								{tableIndexToUpdate}
+								rowIndexToRemove={rowIndex}
+								{getData}
 							/>
 						</td>
 					{/if}
 				</tr>
-				{/each}
-				{/if}
-		</tbody>
-	</table>
+			{/each}
+		{/if}
+	</tbody>
+</table>
 
-	<CmsAddTableRow 
+
+	<CmsAddTableRow
 		prevTableData={tableContent}
-		FBData={FBData}
+		{FBData}
 		FBDocument={'kurser'}
-		getData={getData}
-		tableIndexToUpdate={tableIndexToUpdate}
-		/>
+		{getData}
+		{tableIndexToUpdate}
+	/>
 </div>
 
 <style>
-	.p-rel{
+	.p-rel {
 		position: relative !important;
 	}
 </style>
