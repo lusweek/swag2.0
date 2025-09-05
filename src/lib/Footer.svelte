dcb4a51f1039abe3003e70c22ab86da5f6fa668c
Lukas-i-bastun <lukash@live.se>
2025-09-05 17:09:23 +0200

    Rapportera fel funktion

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,18 +1,31 @@
 <script>
 	import { authStore, authHandlers } from '../stores/authStore';
 	import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
+	import Loader from './Loader.svelte';
 
 	let currentUser;
 	authStore.subscribe((curr) => {
 		currentUser = curr?.currentUser
 	});
 
+  let isLoading = false;
+
+  function handleLoader() {
+    isLoading = true;
+
+    setTimeout(() => {
+      isLoading = false;
+    }, 2000); // 2 sekunder
+  }
+
 </script>
 
 <section class="bg-slate-300 py-16 flex justify-evenly items-center">
 
 	<div class="text-start	">
-		<p>Email: <a href="mailto:swag.goteborg@gmail.com">swag.goteborg@gmail.com</a></p>
+		<p>Email: <a 
+			on:click={handleLoader} 
+			href="mailto:swag.goteborg@gmail.com">swag.goteborg@gmail.com</a></p>
 		<p class="">Telefon: <a href="tel: 0709301799">0709301799</a></p>
 		<p>
 			{#if currentUser}
@@ -21,12 +34,18 @@
 			Admin: <a href="/login">Logga in</a>
 			{/if}
 		</p>
+		<p><a on:click={handleLoader}
+			href="mailto:lukash@live.se?subject=Rapportering av fel på SWAG-hemsidan&body=Jag har upptäckt ett fel på er hemsida. Beskriv exakt vad felet är och skicka gärna med bilder. Tack så mycket!"
+			>Rapportera fel på hemsidan</a>
+		</p>
+
 	</div>
 	<div class="w-9">
 		<a href="https://www.instagram.com/swag_streetworkout/" target="_blank"><FaInstagram /></a>
 		
 	</div>
 
+      <Loader isLoading={isLoading} />
 </section>
 
 <style>
--- src/routes/anmälan/+page.svelte
+++ src/routes/anmälan/+page.svelte
@@ -234,12 +234,12 @@ function fillForm() {
 <h1>Medlemsskap och anmälan</h1>
 
 <CupcakeArticle>
-<h2 class="mb-4">Bli medlem, anmäl dig till kurs eller köp open gym terminspaket.</h2>
-<p class="text-left w-full lg:w-10/12">Medlemsskapet kostar 100 kr och betalas i samband med anmälan till kurs eller open gym terminspaket. 
+<h2 class="m-4">Bli medlem, anmäl dig till kurs eller köp open gym terminspaket.</h2>
+<p class="text-left p-4 w-full lg:w-10/12">Medlemsskapet kostar 100 kr och betalas i samband med anmälan till kurs eller open gym terminspaket. 
 	Du kan även köpa bara medlemsskapet om du vill ha bättre pris på att gå på open gym (60 kr med medlemsskap, annars 80 kr). Medlemsskapet gäller fram tills nyår oavsätt när på året det köps.</p>
-<p class="text-left w-full lg:w-10/12">'Open gym - termin' innebär att du får gå på alla open gym passen under en termin. </p>
+<p class="text-left p-4 w-full lg:w-10/12">'Open gym - termin' innebär att du får gå på alla open gym passen under en termin. </p>
 
-<a href="/anmälan/redanMedlem" class="link my-4">Redan medlem och vill skriva upp dig? Klicka här</a>
+<a href="/anmälan/redanMedlem" class="link p-4 my-4">Redan medlem och vill skriva upp dig? Klicka här</a>
 
 </CupcakeArticle>
 
